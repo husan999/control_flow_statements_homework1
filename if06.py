6e6dc1445f329bb785ed107ea59ff43f1a34263f
husan999 <husanshunqorov07@gmail.com>
2023-03-08 23:18:34 +0500

    good

--- if06.py
+++ if06.py
@@ -13,4 +13,31 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    p = 0
+
+    if a < 0:
+        p += 1
+
+    if b < 0:
+        p += 1
+
+    if c < 0:
+        p += 1
+
+    n = 0
+    if a > 0:
+        n += 1
+
+    if b > 0:
+        n += 1
+
+    if c > 0:
+        n += 1
+
+    if p < n:
+        return "there are a lot of positive numbers"
+
+    return "there are a lot of negative numbers"
+
+
+print(main(-8,9,8))
